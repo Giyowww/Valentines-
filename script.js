9fad754cf454ff821bb9e27b51c9aa9a587712e8
Giyowww <2242743@slu.edu.ph>
2026-02-10 02:32:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,78 +2,74 @@
 
 // Function to handle button click events
 function selectOption(option) {
-    // Check which option was clicked
     if (option === 'yes') {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
-            document.getElementById('name').style.display = 'none'; // Hide the name [NEW]
+            document.getElementById('name').style.display = 'none'; // Hide the name Mikhaela Joyce Lazarte
             displayCatHeart(); // Display the cat-heart.gif
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        // Change text on the "No" button
+        document.getElementById('no-button').innerText = 'You sure?';
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
-        var newSize = parseFloat(currentFontSize) * 2; 
+        var newSize = parseFloat(currentFontSize) * 2;
         yesButton.style.fontSize = newSize + 'px';
     } else {
         alert('Invalid option!');
     }
 }
 
-// Function to flash rainbow colors and then execute a callback function
+// Function to flash rainbow colors
 function flashRainbowColors(callback) {
     var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
     var i = 0;
     var interval = setInterval(function() {
         document.body.style.backgroundColor = colors[i];
         i = (i + 1) % colors.length;
-    }, 200); 
+    }, 200);
     setTimeout(function() {
         clearInterval(interval);
-        document.body.style.backgroundColor = ''; 
+        document.body.style.backgroundColor = '';
         if (callback) {
             callback();
         }
-    }, 2000); 
+    }, 2000);
 }
 
 // Function to display the cat.gif initially
 function displayCat() {
     var imageContainer = document.getElementById('image-container');
     var catImage = new Image();
-    catImage.src = 'cat.gif'; 
+    catImage.src = 'cat.gif';
     catImage.alt = 'Cat';
     catImage.onload = function() {
         imageContainer.appendChild(catImage);
     };
 }
 
-// Function to display the cat-heart.gif
+// Function to display the cat-heart.gif and the new message
 function displayCatHeart() {
-    // Clear existing content in the image container
     document.getElementById('image-container').innerHTML = '';
     var imageContainer = document.getElementById('image-container');
     var catHeartImage = new Image();
-    catHeartImage.src = 'cat-heart.gif'; 
+    catHeartImage.src = 'cat-heart.gif';
     catHeartImage.alt = 'Cat Heart';
     catHeartImage.onload = function() {
         imageContainer.appendChild(catHeartImage);
-        // Hide the options container
         document.getElementById('options').style.display = 'none';
         
-        // Add "Yey love u baby" text [NEW]
-        var message = document.createElement('div');
-        message.id = 'success-message';
-        message.innerText = 'Yey love u baby';
-        message.style.fontFamily = "'Sacramento', cursive";
-        message.style.fontSize = "48px";
-        message.style.marginTop = "20px";
-        document.getElementById('container').appendChild(message);
+        // Add "Yey love u baby" message
+        var loveMessage = document.createElement('div');
+        loveMessage.innerText = 'Yey love u baby';
+        loveMessage.style.fontFamily = "'Sacramento', cursive";
+        loveMessage.style.fontSize = "48px";
+        loveMessage.style.marginTop = "20px";
+        loveMessage.style.color = "#FB607F";
+        document.getElementById('container').appendChild(loveMessage);
     };
 }
 
-// Display the cat.gif initially
 displayCat();
